b7de1f4f8c1d3149ccf99357e060dff067e0decd
Ahmad Alobaid <ahmad88me@gmail.com>
2020-07-14 09:04:59 +0300

    include dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="easysparql", # Replace with your own username
-    version="1.0",
+    version="1.1",
     author="Ahmad Alobaid",
     author_email="aalobaid@fi.upm.es",
     description="A python wrapper to easily query knowledge graphs with SPARQL",
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/oeg-upm/easysparql",
     packages=setuptools.find_packages(),
+    install_requires=[
+          'SPARQLWrapper',
+    ],
     classifiers=[
         "Programming Language :: Python :: 2",
         "License :: OSI Approved :: Apache Software License",
